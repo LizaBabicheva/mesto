cf5364c048ba120a2047bd7d598e121fe493d7db
Elizaveta Babicheva <elizavetababicheva@MacBook-Pro-Elizaveta.local>
2022-05-12 16:41:34 +0300

    fix: убрать дубль поиска переменной

--- script/index.js
+++ script/index.js
@@ -4,6 +4,8 @@ let jobInput = formElement.querySelector('#input-profile-about');
 let nameLabel = document.querySelector('.profile__name');
 let jobLabel = document.querySelector('.profile__about');
 let popup = document.querySelector('.popup');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -12,7 +14,7 @@ function openPopup() {
 }
 
 function closePopup() {
-  document.querySelector('.popup').classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
@@ -24,5 +26,5 @@ function handleFormSubmit (evt) {
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-document.querySelector('.profile__edit-button').addEventListener('click', openPopup);
-document.querySelector('.popup__close-button').addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
